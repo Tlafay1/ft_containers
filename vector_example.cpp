8decd5776c8c6a9bd6a50efc5e99a0bc8cbb1c5a
Tlafay1 <tlclash12@gmail.com>
2022-07-21 17:33:13 +0200

    Some readability changes

--- vector_example.cpp
+++ vector_example.cpp
@@ -1,11 +1,12 @@
-namespace nonstd {
+namespace nonstd
+{
 
 	template<typename T>
 	class vector
 	{
 	public:
-		using iterator = T * ;
-		using const_iterator = const T*;
+		using	iterator = T * ;
+		using	const_iterator = const T*;
 
 		vector();
 		explicit vector(const size_t count);
@@ -14,15 +15,15 @@ namespace nonstd {
 		vector(vector&& other);
 		~vector();
 
-		vector& operator=(const vector& other);
-		vector& operator=(vector&& other);
+		vector&	operator=(const vector& other);
+		vector&	operator=(vector&& other);
 
-		size_t size() const;
-		size_t capacity() const;
+		size_t	size() const;
+		size_t	capacity() const;
 
-		void push_back(const T& val);
-		void push_back(T&& val);
-		void pop_back();
+		void	push_back(const T& val);
+		void	push_back(T&& val);
+		void	pop_back();
 
 		T& 			front();
 		T& 			back();
@@ -31,61 +32,72 @@ namespace nonstd {
 		const T&	back() const;
 		const T&	operator[](const size_t pos) const;
 
-		iterator begin();
-		const_iterator begin() const;
-		iterator end();
-		const_iterator end() const;
+		iterator		begin();
+		const_iterator	begin() const;
+		iterator		end();
+		const_iterator	end() const;
 	private:
-		T * buffer;
-		iterator m_first;
-		iterator m_last;
-		iterator m_end;
+		T			*buffer;
+		iterator	m_first;
+		iterator	m_last;
+		iterator	m_end;
 
-		void realloc(const size_t factor, const size_t carry);
-		void alloc(const size_t cap);
+		void	realloc(const size_t factor, const size_t carry);
+		void	alloc(const size_t cap);
 	};
 
 	template<typename T>
-	vector<T>::vector() : buffer(new T[10]), m_first(buffer), m_last(buffer), m_end(buffer + 10) {
+	vector<T>::vector() : buffer(new T[10]), m_first(buffer), m_last(buffer), m_end(buffer + 10)
+	{
 
 	}
 
 	template<typename T>
-	vector<T>::vector(const size_t count) : buffer(new T[count]), m_first(buffer), m_last(buffer + count), m_end(buffer + count) {
+	vector<T>::vector(const size_t count) : buffer(new T[count]), m_first(buffer), m_last(buffer + count), m_end(buffer + count)
+	{
 
 	}
 
 	template<typename T>
-	vector<T>::vector(const size_t count, const T& val) : buffer(new T[count]), m_first(buffer), m_last(buffer + count), m_end(buffer + count) {
-		while (count--) {
+	vector<T>::vector(const size_t count, const T& val) : buffer(new T[count]), m_first(buffer), m_last(buffer + count), m_end(buffer + count)
+	{
+		while (count--)
+		{
 			buffer[count] = val;
 		}
 	}
 
 	template<typename T>
-	vector<T>::vector(const vector& other) : buffer(new T[other.capacity()]), m_first(buffer), m_last(buffer + other.size()), m_end(buffer + other.capacity()) {
-		for (size_t i = 0; i < size(); ++i) {
+	vector<T>::vector(const vector& other) : buffer(new T[other.capacity()]), m_first(buffer), m_last(buffer + other.size()), m_end(buffer + other.capacity())
+	{
+		for (size_t i = 0; i < size(); ++i)
+		{
 			buffer[i] = other[i];
 		}
 	}
 
 	template<typename T>
-	vector<T>::vector(vector&& other) : buffer(other.buffer), m_first(other.m_first), m_last(other.m_last), m_end(other.m_end) {
+	vector<T>::vector(vector&& other) : buffer(other.buffer), m_first(other.m_first), m_last(other.m_last), m_end(other.m_end)
+	{
 		other.buffer = nullptr;
 		other.m_first = other.m_last = other.m_end = nullptr;
 	}
 
 	template<typename T>
-	vector<T>::~vector() {
-		if (buffer != nullptr) {
+	vector<T>::~vector()
+	{
+		if (buffer != nullptr)
+		{
 			m_first = m_last = m_end = nullptr;
 			delete[] buffer;
 		}
 	}
 
 	template<typename T>
-	vector<T>& vector<T>::operator=(const vector<T>& other) {
-		if (this == &other) {
+	vector<T>& vector<T>::operator=(const vector<T>& other)
+	{
+		if (this == &other)
+		{
 			return *this;
 		}
 		this->~vector();
@@ -93,15 +105,18 @@ namespace nonstd {
 		m_first = buffer;
 		m_last = buffer + other.size();
 		m_end = buffer + other.capacity();
-		for (size_t i = 0; i < size(); ++i) {
+		for (size_t i = 0; i < size(); ++i)
+		{
 			buffer[i] = other[i];
 		}
 		return *this;
 	}
 
 	template<typename T>
-	vector<T>& vector<T>::operator=(vector<T>&& other) {
-		if (this == &other) {
+	vector<T>& vector<T>::operator=(vector<T>&& other)
+	{
+		if (this == &other)
+		{
 			return *this;
 		}
 		this->~vector();
@@ -117,18 +132,22 @@ namespace nonstd {
 	}
 
 	template<typename T>
-	size_t vector<T>::size() const {
+	size_t	vector<T>::size() const
+	{
 		return static_cast<size_t>(m_last - m_first);
 	}
 
 	template<typename T>
-	size_t vector<T>::capacity() const {
+	size_t	vector<T>::capacity() const
+	{
 		return static_cast<size_t>(m_end - m_first);
 	}
 
 	template<typename T>
-	void vector<T>::push_back(const T& val) {
-		if (size() < capacity()) {
+	void	vector<T>::push_back(const T& val)
+	{
+		if (size() < capacity())
+		{
 			*(m_last++) = val;
 			return;
 		}
@@ -137,8 +156,10 @@ namespace nonstd {
 	}
 
 	template<typename T>
-	void vector<T>::push_back(T&& val) {
-		if (size() < capacity()) {
+	void	vector<T>::push_back(T&& val)
+	{
+		if (size() < capacity())
+		{
 			*(m_last++) = std::move(val);
 			return;
 		}
@@ -147,91 +168,112 @@ namespace nonstd {
 	}
 
 	template<typename T>
-	void vector<T>::pop_back() {
-		if (size() == 0) {
+	void	vector<T>::pop_back()
+	{
+		if (size() == 0)
+		{
 			throw std::exception("vector is empty");
 		}
 		(--m_last)->~T();
 	}
 
 	template<typename T>
-	T& vector<T>::front() {
-		if (size() == 0) {
+	T&	vector<T>::front()
+	{
+		if (size() == 0)
+		{
 			throw std::exception("front(): vector is empty");
 		}
 		return *begin();
 	}
 
 	template<typename T>
-	const T& vector<T>::front() const {
-		if (size() == 0) {
+	const T&	vector<T>::front() const
+	{
+		if (size() == 0)
+		{
 			throw std::exception("front(): vector is empty");
 		}
 		return *begin();
 	}
 
 	template<typename T>
-	T& vector<T>::back() {
-		if (size() == 0) {
+	T&	vector<T>::back()
+	{
+		if (size() == 0)
+		{
 			throw std::exception("back(): vector is empty");
 		}
 		return *(end() - 1);
 	}
 
 	template<typename T>
-	const T& vector<T>::back() const {
-		if (size() == 0) {
+	const T&	vector<T>::back() const
+	{
+		if (size() == 0)
+		{
 			throw std::exception("back(): vector is empty");
 		}
 		return *(end() - 1);
 	}
 
 	template<typename T>
-	T& vector<T>::operator[](const size_t pos) {
-		if (pos >= size()) {
+	T&	vector<T>::operator[](const size_t pos)
+	{
+		if (pos >= size())
+		{
 			throw std::exception("index out of range");
 		}
 		return buffer[pos];
 	}
 
 	template<typename T>
-	const T& vector<T>::operator[](const size_t pos) const {
-		if (pos >= size()) {
+	const T&	vector<T>::operator[](const size_t pos) const
+	{
+		if (pos >= size())
+		{
 			throw std::exception("index out of range");
 		}
 		return buffer[pos];
 	}
 
 	template<typename T>
-	typename vector<T>::iterator vector<T>::begin() {
+	typename vector<T>::iterator	vector<T>::begin()
+	{
 		return m_first;
 	}
 
 	template<typename T>
-	typename vector<T>::iterator vector<T>::end() {
+	typename vector<T>::iterator	vector<T>::end()
+	{
 		return m_last;
 	}
 
 	template<typename T>
-	typename vector<T>::const_iterator vector<T>::begin() const {
+	typename vector<T>::const_iterator	vector<T>::begin() const
+	{
 		return m_first;
 	}
 
 	template<typename T>
-	typename vector<T>::const_iterator vector<T>::end() const {
+	typename vector<T>::const_iterator	vector<T>::end() const
+	{
 		return m_last;
 	}
 
 	template<typename T>
-	void vector<T>::realloc(const size_t factor, const size_t carry) {
+	void	vector<T>::realloc(const size_t factor, const size_t carry)
+	{
 		alloc(capacity() * factor + carry);
 	}
 
 	template<typename T>
-	void vector<T>::alloc(const size_t cap) {
+	void	vector<T>::alloc(const size_t cap)
+	{
 		T* new_buffer = new T[cap];
 		size_t sz = size();
-		for (size_t i = 0; i < sz; ++i) {
+		for (size_t i = 0; i < sz; ++i)
+		{
 			new_buffer[i] = buffer[i];
 		}
 		this->~vector();
