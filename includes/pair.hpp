01706e7836990ff28457d04b4d28da4249cca572
Tlafay1 <tlclash12@gmail.com>
2022-09-22 17:35:32 +0200

    Make pair done, last overload operators

--- includes/pair.hpp
+++ includes/pair.hpp
@@ -24,9 +24,12 @@ namespace ft
 		first_type	first;
 		second_type	second;
 	};
-}
 
-template< class T1, class T2 >
-std::pair<T1, T2> make_pair(T1 t, T2 u);
+	template< class T1, class T2 >
+	pair<T1, T2>	make_pair(T1 t, T2 u)
+	{
+		return (pair<T1, T2>(t, u));
+	}
+}
 
 #endif
