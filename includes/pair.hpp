19ca55734156f7ef708a1a1e5bc6c81d07c65d8b
Tlafay1 <tlclash12@gmail.com>
2022-09-22 17:31:57 +0200

    Class made, make_pair next

--- includes/pair.hpp
+++ includes/pair.hpp
@@ -9,12 +9,17 @@ namespace ft
 		typedef T1 first_type;
 		typedef T2 second_type;
 
-		pair();
-		pair(const T1 &x, const T2 &y);
+		pair(): first(T1()), second(T2()) {}
+		pair(const T1 &x, const T2 &y): first(x), second(y) {}
 		template<class U1, class U2>
-		pair(const pair<U1, U2> &p);
+		pair(const pair<U1, U2> &p) { *this = p; }
 
-		pair	&operator=(const pair &other);
+		pair	&operator=(const pair &other)
+		{
+			first = other.first;
+			second = other.second;
+			return (*this);
+		}
 
 		first_type	first;
 		second_type	second;
@@ -22,6 +27,6 @@ namespace ft
 }
 
 template< class T1, class T2 >
-std::pair<T1, T2> make_pair(T1 t, T2 u);0
+std::pair<T1, T2> make_pair(T1 t, T2 u);
 
 #endif
