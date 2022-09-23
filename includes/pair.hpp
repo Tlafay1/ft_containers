916d84795e9807335a7054895d40da934916f68a
Tlafay1 <tlclash12@gmail.com>
2022-09-23 07:28:05 -0400

    Pair fully done

--- includes/pair.hpp
+++ includes/pair.hpp
@@ -25,10 +25,48 @@ namespace ft
 		second_type	second;
 	};
 
-	template< class T1, class T2 >
+	template<class T1, class T2>
 	pair<T1, T2>	make_pair(T1 t, T2 u)
 	{
 		return (pair<T1, T2>(t, u));
+	}
+
+	template<class T1, class T2>
+	bool operator==(const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs)
+	{
+		return (lhs.first == rhs.first && lhs.second == rhs.second);
+	}
+
+	template<class T1, class T2>
+	bool operator!=(const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs)
+	{
+		return (!(lhs == rhs));
+	}
+
+	template<class T1, class T2>
+	bool operator<(const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs)
+	{
+		return ((lhs.first != rhs.first) ?
+			lhs.first < rhs.first : lhs.second < rhs.second);
+	}
+
+	template<class T1, class T2>
+	bool operator<=(const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs)
+	{
+		return (!(lhs > rhs));
+	}
+
+	template<class T1, class T2>
+	bool operator>(const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs)
+	{
+		return ((lhs.first != rhs.first) ?
+			lhs.first < rhs.first : lhs.second < rhs.second);
+	}
+
+	template<class T1, class T2>
+	bool operator>=(const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs)
+	{
+		return (!(lhs < rhs));
 	}
 }
 
