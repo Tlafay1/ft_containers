3155d5136351af04fee54e7a63eadbae588773dd
Tlafay1 <tlclash12@gmail.com>
2022-09-27 03:54:03 -0400

    forgot to push

--- includes/map.hpp
+++ includes/map.hpp
@@ -2,13 +2,32 @@
 # define MAP_HPP
 
 #include "pair.hpp"
+#include "iterator_traits.hpp"
+#include "reverse_iterator.hpp"
 
-// namespace ft
-// {
-// 	class map
-// 	{
-		
-// 	};
-// }
+namespace ft
+{
+	template< class Key, class T, class Compare = std::less<Key>,
+		class Allocator = std::allocator< std::pair< const Key, T> > >
+	class map
+	{
+		typedef key_type										Key;
+		typedef	T												mapped_type;
+		typename typedef ft::pair<const Key, T>					value_type;
+		typedef	std::size_t										size_type;
+		typedef	ft::iterator_traits<iterator>::difference_type	difference_type;
+		typedef	Compare											key_compare;
+		typedef	Allocator										allocator_type;
+		typedef	value_type										&reference;
+		typedef	const value_type								&const_reference;
+		typename typedef Allocator::pointer						pointer; 
+		typename typedef Allocator::const_pointer				const_pointer;	 
+		typename typedef ft::reverse_iterator<iterator>			reverse_iterator;
+		typename typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
+
+	};
+}
+
+#include "map.tpp"
 
 #endif
